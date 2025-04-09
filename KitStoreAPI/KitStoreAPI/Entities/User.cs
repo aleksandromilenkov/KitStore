3382aac662a24ac81cf644d8ca95d6b69f921a52
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-04-09 22:09:54 +0200

    creating react with vite and typescript

--- KitStoreAPI/KitStoreAPI/Entities/User.cs
+++ KitStoreAPI/KitStoreAPI/Entities/User.cs
@@ -5,7 +5,6 @@ namespace KitStoreAPI.Entities
 {
     public class User : IdentityUser
     {
-        public int? AddressId { get; set; }
         public Address? Address { get; set; }
         public string? PictureUrl { get; set; }
         public string? PublicId { get; set; }
---
+++ KitStoreClient/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <p>KitStore</p>
+    </div>
+  )
+}
+
+export default App
