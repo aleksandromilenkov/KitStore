2708b0f30a09bb6fe868ed1db60b8c8bdb38ff74
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2025-04-14 00:20:08 +0200

    home and about page

--- KitStoreClient/src/features/about/AboutPage.tsx
+++ KitStoreClient/src/features/about/AboutPage.tsx
@@ -1,6 +1,47 @@
+import { Container, Typography, Box, Card, CardContent } from "@mui/material";
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
-export default AboutPage
+    <Container maxWidth="md" sx={{ pb: "10px" }}>
+      <Box textAlign="center" my={4}>
+        <Typography variant="h3" fontWeight="bold" gutterBottom>
+          About Us
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          Your go-to destination for authentic football kits worldwide.
+        </Typography>
+      </Box>
+
+      <Card sx={{ mb: 4, boxShadow: 3 }}>
+        <CardContent>
+          <Typography variant="h5" fontWeight="bold" gutterBottom>
+            Who We Are
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            At KitStore, we live and breathe football. We bring you the latest
+            official home, away, and third kits from your favorite clubs and
+            national teams. From iconic classics to this season’s fresh drops,
+            we make sure every fan finds their perfect fit.
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Card sx={{ mb: 4, boxShadow: 3 }}>
+        <CardContent>
+          <Typography variant="h5" fontWeight="bold" gutterBottom>
+            Our Mission
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            We aim to unite fans around the world through the love of the game.
+            Our mission is to provide high-quality, 100% authentic football kits
+            that celebrate the passion, pride, and history of football culture.
+            Whether you’re in the stands, on the pitch, or at home—we’ve got
+            the gear that lets you rep your team with pride.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
+
+export default AboutPage;
--- KitStoreClient/src/features/home/HomePage.tsx
+++ KitStoreClient/src/features/home/HomePage.tsx
@@ -1,6 +1,54 @@
+import { Box, Button, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box maxWidth="xl" mx="auto" px={4} position="relative">
+      <Box display="flex" height={"100vh"} flexDirection="column" alignItems="center" justifyContent="center" position="relative">
+        <img src="/images/hero1.png" alt="hero image kits"
+         style={{
+          position:"absolute",
+          inset: 0,
+          width:"100%",
+          height:"100%",
+          objectFit:"cover",
+          borderRadius:'16px',
+          zIndex:0
+          }}/>
+          <Box display="flex" flexDirection="column" p={8} alignItems="center" position="relative" borderRadius={4}>
+            <Typography
+              variant="h1"
+              color="white"
+              fontWeight="bold"
+              textAlign="center"
+              my={3}
+              sx={{
+                textShadow: "7px 7px 8px rgba(0,0,0,0.8)",
+              }}
+            >
+              Welcome to KitStore!
+            </Typography>
+            <Button
+            variant="contained"
+            size="large"
+            component={Link}
+            to="/catalog"
+            sx={{
+              mt:8,
+              backgroundImage: "linear-gradient(to right, #2563eb, #06b6d4",
+              fontWeight:"bold",
+              color:"white",
+              borderRadius:"16px",
+              px:8,
+              py:2,
+              border:"2px solid transparent"
+            }}
+            >
+              Go To Shop
+            </Button>
+          </Box>
+      </Box>
+    </Box>
   )
 }
 export default HomePage
